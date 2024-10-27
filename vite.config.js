ab0229ff4d95123c96cd1ff4db2cfb223ad41d2f
wmonsalv <williammonsalve923@gmail.com>
2024-10-27 19:58:19 -0400

    json and vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Portfolio_Project/', // This ensures assets load correctly on GitHub Pages
+});
