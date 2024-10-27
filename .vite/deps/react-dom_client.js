9570b31a5ff896fc45184608480e70338515bf61
wmonsalv <williammonsalve923@gmail.com>
2024-10-27 19:43:11 -0400

    small change

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,7 @@
+import {
+  require_client
+} from "./chunk-6SRDACTQ.js";
+import "./chunk-FLAVOKRJ.js";
+import "./chunk-AC2VUBZ6.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map
